2ad7555b2d4b64bc124a02c7a6774f6526433902
Mats Rauhala <mats.rauhala@iki.fi>
2019-03-21 23:29:39 +0200

    Formatting changes

--- src/XMonad/Config/MasseR/ExtraConfig.hs
+++ src/XMonad/Config/MasseR/ExtraConfig.hs
@@ -4,11 +4,22 @@ module XMonad.Config.MasseR.ExtraConfig where
 import GHC.Generics (Generic)
 import Data.Text (Text)
 
-data Applications = Applications { browser :: FilePath, launcher :: FilePath, prompt :: FilePath, screenshot :: FilePath, urxvt :: FilePath } deriving (Show, Generic)
+data Applications =
+  Applications { browser :: FilePath
+               , launcher :: FilePath
+               , prompt :: FilePath
+               , screenshot :: FilePath
+               , urxvt :: FilePath
+               } deriving (Show, Generic)
 
-data TopicRule = TopicRule { topicName :: Text
-                           , topicHome :: Maybe Text
-                           , topicAction :: Maybe Text }
-                           deriving (Show, Generic)
+data TopicRule =
+  TopicRule { topicName :: Text
+            , topicHome :: Maybe Text
+            , topicAction :: Maybe Text
+            }
+               deriving (Show, Generic)
 
-data ExtraConfig = ExtraConfig { applications :: Applications, topics :: [ TopicRule ] } deriving (Show, Generic)
+data ExtraConfig =
+  ExtraConfig { applications :: Applications
+              , topics :: [ TopicRule ] }
+                 deriving (Show, Generic)
