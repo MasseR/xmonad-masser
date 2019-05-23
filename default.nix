6c9bc7399a607e207effa5cf2415c0821f46e727
Mats Rauhala <mats.rauhala@iki.fi>
2019-05-23 12:22:14 +0300

    Dev tooling

--- default.nix
+++ default.nix
@@ -1,3 +1,13 @@
-{ haskellPackages }:
+{ lib, haskellPackages }:
 
-haskellPackages.callCabal2nix "xmonad-masser" ./. {}
+let
+  filtered = src: lib.sourceByRegex src [
+    "^src.*"
+    "Setup.hs"
+    ".*cabal"
+    "LICENSE"
+    ];
+
+in
+
+haskellPackages.callCabal2nix "xmonad-masser" (filtered ./.) {}
