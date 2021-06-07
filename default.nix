b4747a73fda2e264ddcc9e98c6bf3e8d5a9572f5
Mats Rauhala <mats.rauhala@iki.fi>
2021-06-07 15:56:06 +0300

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,15 +1,14 @@
-{ mkDerivation, base, containers, data-default, dhall, directory
-, filepath, generic-lens, lens, mtl, stdenv, text, time, unix
-, xmonad, xmonad-contrib
+{ mkDerivation, base, containers, data-default, directory, filepath
+, generic-lens, lens, lib, mtl, text, time, unix, xmonad
+, xmonad-contrib
 }:
 mkDerivation {
   pname = "xmonad-masser";
   version = "0.1.0.0";
   src = ./.;
-  enableSeparateDataOutput = true;
   libraryHaskellDepends = [
-    base containers data-default dhall directory filepath generic-lens
-    lens mtl text time unix xmonad xmonad-contrib
+    base containers data-default directory filepath generic-lens lens
+    mtl text time unix xmonad xmonad-contrib
   ];
-  license = stdenv.lib.licenses.bsd3;
+  license = lib.licenses.bsd3;
 }
